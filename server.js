d204c85b40a31fb11b93b282d2df5f6992113398
Luca Meneghetti <126782219+lucamenee@users.noreply.github.com>
2024-10-26 11:00:50 +0200

    API_KEY

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
+require('dotenv').config();
 const express = require('express');
 const pool = require('./db');
 const port = 1337;
-
 const app = express();
+const API_KEY = process.env.API_KEY;
 
 app.use(express.json());
+
+// Middleware to check API key
+app.use((req, res, next) => {
+    const apiKey = req.headers['api-key'];
+    console.log("------------------------------------------------");
+    console.log("New request for: "+req.url );
+    console.log("Method: "+req.method);
+    if (apiKey && apiKey === API_KEY) {
+        next();
+        console.log('API key is valid');        
+    } else {
+        res.status(403).send('Forbidden');
+        console.log('API key is invalid');
+    }
+});
 
 //routes
 app.get('/', (req, res) => {
@@ -13,7 +29,9 @@ app.get('/', (req, res) => {
 
 app.get('/tags', async (req, res) => {
     try {
+        console.log("querying tags");
         const data = await pool.query('select * from tags');
+        console.log(data.rows);
         res.status(200).send(data.rows);
     } catch (error) {
         console.error(error.message);

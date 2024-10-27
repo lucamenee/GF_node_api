cd51703e8c447fab3eebce4c10ea66c92ec7bd30
Luca Meneghetti <126782219+lucamenee@users.noreply.github.com>
2024-10-27 12:16:19 +0100

    generic select query function, inventory endpoint

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
+/**
+ *  Simple HTTP REST server + MongoDB (Mongoose) + Express
+ * 
+ *  Endpoints          Attributes          Method        Description
+ * 
+ *     /                  -                  GET         Returns the version and a list of available endpoints
+ *     /inventory       ?id=                 GET         Returns all the inventory items for a given id
+ *
+ *     /tags              -                  GET         Get a list of tags
+ * 
+ *     /login          ?username=&password=  POST        login an existing user, returning bool (for now)
+ *    
+ * 
+ *     ...
+ * 
+ **/
+
 require('dotenv').config();
 const express = require('express');
 const pool = require('./db');
@@ -22,22 +39,61 @@ app.use((req, res, next) => {
     }
 });
 
+
 //routes
 app.get('/', (req, res) => {
-    res.sendStatus(200);
+    res.sendStatus(200).json( { api_version: "1.0", endpoints: ["/login", "/tags", "/inventory"] } );
 });
 
+// function to create a generic select query (pass pars in the order they are in the query)
+function genericSelectQuery(query, pars) {
+    return async (req, res) => {
+        try {
+            console.log("querying");
+            const data = await pool.query(query, pars);
+            console.log(data.rows);
+            res.status(200).send(data.rows);
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+}
+
+// return all the tags in the db
 app.get('/tags', async (req, res) => {
+    genericSelectQuery('select * from tags') (req, res);
+})
+
+// return all the foods in the db
+app.get('/alimenti', async (req, res) => {
+    genericSelectQuery('select * from alimenti') (req, res);
+})
+
+// return all the info about an invetory for a given id
+app.get('/inventory', async (req, res) => {
+    genericSelectQuery('select * from inventari natural left join righe_inventario ' +
+            'natural join alimenti natural join categorie where id_inventario = $1', [req.query.id]) (req, res);
+})
+
+
+
+
+
+
+
+// to delete, keep only for popolate the db
+app.post('/popolate', async (req, res) => {
     try {
-        console.log("querying tags");
-        const data = await pool.query('select * from tags');
-        console.log(data.rows);
-        res.status(200).send(data.rows);
+        console.log("populating db");
+        //await pool.query("insert into righe_inventario(id_inventario, id_alimento, data_scadenza, grammi, essenziale) values (1, 3, '2024-12-18', 300, false)");
+            
+        res.status(200).send("inventory added");
     } catch (error) {
         console.error(error.message);
     }
 })
 
+// to delete, keep only for reference for post requests
 app.post('/tags', async (req, res) => {
     try {
         console.log(req.query.tag);
